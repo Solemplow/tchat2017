817e363a0d124ae5f55b6c265057dc733a13bd2f
solem <tridegand@gmail.com>
2017-05-12 16:00:24 +0200

    retry

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("DB_HOST", "localhost"); 
+define("DB_LOGIN", "root"); 
+define("DB_PASS", ""); 
+define("DB_NAME", "sql8614_tchat"); 
+define("DB_CHARSET", "utf8");
+
+define("AJAX_REFRESH", 3);
